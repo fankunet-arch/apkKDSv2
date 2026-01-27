9a037798121af8840aa114b9c1bd01185abd7387
fankunet-arch <fankunet@gmail.com>
2026-01-28 00:38:47 +0100

    Add files via upload

---
+++ MyApplication/TOPTEAKDS/build.gradle.kts
@@ -0,0 +1,8 @@
+/*
+ * 文件名: build.gradle.kts (Project: TOPTEA_KDS)
+ * 描述: (这是第 2 步)
+ */
+plugins {
+    id("com.android.application") version "8.4.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+}
---
+++ MyApplication/TOPTEAKDS/settings.gradle.kts
@@ -0,0 +1,23 @@
+/*
+ * 文件名: settings.gradle.kts
+ * 描述: (这是第 1 步)
+ */
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TOPTEA_KDS"
+include(":app")
